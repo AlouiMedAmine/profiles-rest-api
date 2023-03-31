ae5e35397e368e06abd53f92ae7a63cacb099c7b
AlouiMedAmine <mohammed.Aminaloui@gmail.com>
2023-03-31 17:19:53 +0200

    add info

---
+++ my-project-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amine/Documents/Courses/profiles-rest-api/my-project-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
